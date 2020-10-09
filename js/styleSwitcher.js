43325adcc9cd0322a4727be3f95bec2a5133aede
kamalelazizialaoui <elazizialaouikamal@yahoo.com>
2020-10-09 15:00:45 -0400

    modification

--- js/styleSwitcher.js
+++ js/styleSwitcher.js
@@ -3,11 +3,24 @@ const links =document.querySelectorAll(".alternate-style");
 
 function setActiveStyle(color){
   for(let i=0; i< links.length; i++){
-      if(color == links[i].getAttribute('title')){
+      if(color ===links[i].getAttribute('title')){
           links[i].removeAttribute("disabled");
       }
       else{
           links[i].setAttribute("disabled", "true");
       }
   }
+}
+
+//body skin
+const bodySkin = document.querySelectorAll(".body-skin")
+for(var i=0; i< bodySkin.length; i++){
+    bodySkin[i].addEventListener("change", function(){
+       if(this.value === "dark"){
+           document.body.className="dark"
+       }
+       else{
+        document.body.className=""
+       }
+    })
 }
